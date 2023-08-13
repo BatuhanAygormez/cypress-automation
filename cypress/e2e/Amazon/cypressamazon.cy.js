5eccffa1698b1777d379cc1a7427c766b01405c7
Batuhan <Batuhan@DESKTOP-HQ5GJUF>
2023-08-13 12:32:08 +0300

    Add project files.

---
+++ cypress/e2e/Amazon/cypressamazon.cy.js
@@ -0,0 +1,18 @@
+﻿describe('Amazon Giriş ve Arama Testi', () => {
+    it('Amazon ana sayfasını ziyaret eder ve arama yapar', () => {
+        cy.visit('https://www.amazon.com.tr/'); //amazon trye gider
+        cy.get('#twotabsearchtextbox').type('akıllı telefon'); // Arama kutusuna metin yazma
+        cy.get('.nav-input').click({ multiple: true }); // Arama düğmesine tıklama
+
+        // İlgili ürünlerin listelendiğini ve sonuçları kontrol etmek için gerekli kodları ekleyebilirsiniz.
+    });
+});
+//describe('Google Arama Testi', () => {
+//    it('Google ana sayfasını ziyaret eder ve arama yapar', () => {
+//        cy.visit('https://www.google.com/');
+
+//        cy.get('[name="q"]').type('qwrqwrqwronu'); // Arama kutusuna metin yazma
+//        cy.get('[name="btnK"]').eq(1).click({ multiple: true }) // Google Arama düğmesine tıklama
+
+//    });
+//});
