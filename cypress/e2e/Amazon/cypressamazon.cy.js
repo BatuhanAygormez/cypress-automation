2cf1aee3d4dd070cd2f7622df3a758f81f8c59f9
BatuhanAygormez <32410341+BatuhanAygormez@users.noreply.github.com>
2023-08-13 12:41:32 +0300

    Update cypressamazon.cy.js

--- cypress/e2e/Amazon/cypressamazon.cy.js
+++ cypress/e2e/Amazon/cypressamazon.cy.js
@@ -1,4 +1,4 @@
-﻿describe('Amazon Giriş ve Arama Testi', () => {
+describe('Amazon Giriş ve Arama Testi', () => {
     it('Amazon ana sayfasını ziyaret eder ve arama yapar', () => {
         cy.visit('https://www.amazon.com.tr/'); //amazon trye gider
         cy.get('#twotabsearchtextbox').type('akıllı telefon'); // Arama kutusuna metin yazma
@@ -7,12 +7,12 @@
         // İlgili ürünlerin listelendiğini ve sonuçları kontrol etmek için gerekli kodları ekleyebilirsiniz.
     });
 });
-//describe('Google Arama Testi', () => {
-//    it('Google ana sayfasını ziyaret eder ve arama yapar', () => {
-//        cy.visit('https://www.google.com/');
+describe('Google Arama Testi', () => {
+    it('Google ana sayfasını ziyaret eder ve arama yapar', () => {
+        cy.visit('https://www.google.com/');
 
-//        cy.get('[name="q"]').type('qwrqwrqwronu'); // Arama kutusuna metin yazma
-//        cy.get('[name="btnK"]').eq(1).click({ multiple: true }) // Google Arama düğmesine tıklama
+        cy.get('[name="q"]').type('qwrqwrqwronu'); // Arama kutusuna metin yazma
+        cy.get('[name="btnK"]').eq(1).click({ multiple: true }) // Google Arama düğmesine tıklama
 
-//    });
-//});
+    });
+});
