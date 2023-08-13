00e8e77898206170e99999809dc812cc6bf7a106
BatuhanAygormez <32410341+BatuhanAygormez@users.noreply.github.com>
2023-08-14 00:19:47 +0300

    Update cypressamazon.cy.js

--- cypress/e2e/Amazon/cypressamazon.cy.js
+++ cypress/e2e/Amazon/cypressamazon.cy.js
@@ -3,16 +3,16 @@ describe('Amazon Giriş ve Arama Testi', () => {
         cy.visit('https://www.amazon.com.tr/'); //amazon trye gider
         cy.get('#twotabsearchtextbox').type('akıllı telefon'); // Arama kutusuna metin yazma
         cy.get('.nav-input').click({ multiple: true }); // Arama düğmesine tıklama
-
-        // İlgili ürünlerin listelendiğini ve sonuçları kontrol etmek için gerekli kodları ekleyebilirsiniz.
     });
 });
+
 describe('Google Arama Testi', () => {
     it('Google ana sayfasını ziyaret eder ve arama yapar', () => {
         cy.visit('https://www.google.com/');
 
-        cy.get('[name="q"]').type('qwrqwrqwronu'); // Arama kutusuna metin yazma
-        cy.get('[name="btnK"]').eq(1).click({ multiple: true }) // Google Arama düğmesine tıklama
-
+        cy.get('[name="q"]').type('Batuhan Aygörmez Linkedin'); // Arama kutusuna metin yazma
+        cy.get('input[value="Google\'da Ara"]').first().click(); //google da arama butonuna tıklar
+        cy.contains('h3', 'Batuhan Aygörmez - Software Test Engineer').click().should('be.visible'); 
     });
 });
+
